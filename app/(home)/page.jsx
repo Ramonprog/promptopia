4f1480b163314034a74836fd19f3c0a6e1bd8e06
Ramonprog <programador.ramon@gmail.com>
2023-12-05 08:45:51 -0300

    feat: main title

--- app/(home)/page.jsx
+++ app/(home)/page.jsx
@@ -1,7 +1,16 @@
 export default function Home() {
   return (
-    <>
-      <div>teste</div>
-    </>
+    <section className="w-full flex-col flex-center">
+      <h1 className="head_text text-center">
+        Discover & Share
+        <br className="max-md:hidden" />
+        <span className="orange_gradient text-center">AI-Powred Propts</span>
+      </h1>
+      <p className="desc text-center">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi cum
+        quidem porro excepturi esse possimus quisquam. Earum consequatur, maxime
+        minus quis saepe tempore.
+      </p>
+    </section>
   );
 }
